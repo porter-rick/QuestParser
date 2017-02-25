292a4c7bc9bcdfea69f5a283b12d7f1211231e84
porter-rick <porter.rick@gmail.com>
2017-02-25 00:52:14 -0600

    Add files via upload
    Adding mostly empty project

---
+++ QuestParser/QuestParser/Tokens.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestParser
+{
+    public enum TokenType
+    {
+        None = 0,
+        Word,
+        Integer,
+        String,
+        Symbol,
+    }
+
+    public class Tokens
+    {
+
+    }
+}
+}
